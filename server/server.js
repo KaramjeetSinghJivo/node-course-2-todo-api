1c4a0299e10327670d04b288cbe0d7143775c122
KaramjeetSinghJivo <karamjeet.jivo@gmail.com>
2018-06-21 09:11:59 +0530

    Added Get /todo/:id Functionality for Todo App

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -29,6 +30,24 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+//Get /todos/343534645
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id))
+    {
+        res.status(404).send('Id Not Valid');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo)
+        {
+            return res.status(404).send('ID not Found');
+        }
+
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
+})
 
 app.listen(3000, () => {
     console.log('Started on Port 3000');
