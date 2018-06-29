5293b3885b3f1f3942e9a93e4de30c70ca260a20
KaramjeetSinghJivo <karamjeet.jivo@gmail.com>
2018-06-29 22:38:43 +0530

    Add Tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -222,7 +222,7 @@ describe('POST /users', () => {
                     expect(user).toBeTruthy();
                     expect(user.password).not.toBe(password);
                     done();
-                })
+                }).catch((e) => done(e));
             });
     })
 
@@ -246,5 +246,58 @@ describe('POST /users', () => {
             })
             .expect(404)
             .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('Should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.header['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if(err)
+                {
+                    return done(err);
+                }
+
+                Users.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toMatchObject({   // use toMatchObject() instead of toInclude()
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('Should Reject Invalid Login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.header['x-auth']).toBeFalsy();
+            })
+            .end((err, res) => {
+                if(err)
+                {
+                    return done(err);
+                }
+
+                Users.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
     })
 })
