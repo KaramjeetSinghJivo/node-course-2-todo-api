7bf2b7c7c6df25a66018484d4b8505bd7a2be134
KaramjeetSinghJivo <karamjeet.jivo@gmail.com>
2018-06-29 09:07:18 +0530

    Add POST /users/login routes

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,30 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+     
+    return User.findOne({email}).then((user) => {
+        if(!user)
+        {
+            return Promnise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res)
+                {
+                    resolve(user);
+                }
+                else
+                {
+                    reject();
+                }
+            })
+        });
+    })
+}
+
 UserSchema.pre('save', function(next) {
     var user = this;
 
